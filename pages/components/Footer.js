944b2748da93976cb60bb1dba7395aa525420b08
joud-alnsour <alnsour.joud@gmail.com>
2022-06-08 18:10:27 +0300

    add

---
+++ pages/components/Footer.js
@@ -0,0 +1,8 @@
+function Footer() {
+    return (
+      <footer className="px-8 py-8 text-white bg-gray-500">
+          <p>&copy; 2021</p>
+      </footer>
+    )
+  }
+  
---
+++ pages/components/Header.js
@@ -0,0 +1,12 @@
+function Header(){
+    return(
+        <header className="px-8 py-6 bg-gray-500">
+            <h1 className="text-4xl text-white">Cookie Stand Admin</h1>
+
+        </header>
+    )
+}
+
+export default Header
+
+
